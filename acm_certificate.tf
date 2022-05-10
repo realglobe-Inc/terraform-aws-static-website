bfafd49ea304295b3eaf007bc31cf8d127af973e
Tiago Carrondo <tcarrondo@angulosolido.pt>
2022-05-10 10:58:28 +0100

    improve acm cert validation

--- acm_certificate.tf
+++ acm_certificate.tf
@@ -23,5 +23,5 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_acm_certificate_validation" "cert" {
   provider = "aws.us-east-1"
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
