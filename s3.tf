dd488c36a3c976bc19f50a31b474d789467da6dd
Tiago Carrondo <tcarrondo@angulosolido.pt>
2022-05-10 12:30:41 +0100

    fix deprecated arguments

--- s3.tf
+++ s3.tf
@@ -18,7 +18,15 @@ data "aws_iam_policy_document" "bucket_policy" {
 
 resource "aws_s3_bucket" "hosting" {
   bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_bucket_policy" "hosting" {
+  bucket = var.s3_bucket_name
   policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+resource "aws_s3_bucket_cors_configuration" "hosting" {
+  bucket = aws_s3_bucket.hosting.id
 
   dynamic "cors_rule" {
     for_each = [for origin in var.cors_allowed_origins : {
@@ -34,7 +42,7 @@ resource "aws_s3_bucket" "hosting" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
+resource "aws_s3_bucket_versioning" "hosting" {
   bucket = aws_s3_bucket.hosting.id
 
   versioning_configuration {
